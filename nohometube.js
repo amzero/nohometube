b8eb7d83e30b5a2c7d56763ce2fe93702804b8c6
Angelo Morgan <angelomorgan@hotmail.com>
2017-04-22 14:48:13 -0300

    nohometube.js
    Better code.

--- nohometube.js
+++ nohometube.js
@@ -1,35 +1,22 @@
-var funcao = function() {
-    var node = document.getElementById("browse-items-primary");
-	if (node && node.parentNode) {
-	  node.parentNode.removeChild(node);
-	}
-
-	node = document.getElementById("masthead-appbar");
-	if (node && node.parentNode) {
-		 node.parentNode.removeChild(node);
-	}
-		
-	node = document.getElementById("feed");
-	if (node && node.parentNode) {
-		 node.parentNode.removeChild(node);
-	}
+var ids = ["browse-items-primary", "masthead-appbar", "feed"];
+var funcao = function () {
+    for (i = 0; i < ids.length; i++) {
+        node = document.getElementById(ids[i]);
+        if (node && node.parentNode) {
+            node.parentNode.removeChild(node);
+        }
+    }
 };
-
-// seleciona o nó alvo #page-container
-var target = document.querySelector('#page-container');
- 
-// cria uma nova instância de observador
-var observer = new MutationObserver(function(mutations) {
-  mutations.forEach(function(mutation) {
-    //console.log(mutation.type);
-    //alert("FOI");
-    funcao();
-  });    
+var target = document.querySelector('#body-container');
+var observer = new MutationObserver(function (mutations) {
+    mutations.forEach(function (mutation) {
+        funcao();
+    });
 });
- 
-// configuração do observador:
-var config = { attributes: true, childList: true, characterData: true, subtree: true };
-//var config = { childList: true, subtree: true };
- 
-// passar o nó alvo, bem como as opções de observação
+var config = {
+    attributes: true
+    , childList: true
+    , characterData: true
+    , subtree: true
+};
 observer.observe(target, config);
